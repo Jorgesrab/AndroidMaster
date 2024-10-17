5a638e3fbc8011e95fb7d605eae9366ea51ab31c
Jorgesrab <jorge.sanchez174@educa.madrid.org>
2024-10-17 13:42:10 +0200

    commit

---
+++ app/src/main/java/com/example/androidmaster/Variables.kt
@@ -0,0 +1,54 @@
+package com.example.androidmaster
+
+/**
+ *VARIABLES
+ */
+/**
+ * Val = constante (No se puede modificar)| Var = variable (Se puede modificar)
+ */
+
+
+
+fun main() {
+    /**
+     *Variables numericas
+     */
+
+    //Int
+    val entero: Int = 1232;
+    //Float
+    var decimal:Float = 123.342f;
+    //Double
+    val doble:Double = 123.12;
+
+    /**
+     *Variables Alfanumericas
+     */
+
+
+    //Char
+    var charExample:Char = 'a';
+    //String
+    val stringExample:String = "Hola buenas";
+    val stringExample2:String = " me llamo Jorge";
+    val stringExample3:String = "$stringExample$stringExample2 y tengo $decimal años";
+    println(stringExample3)
+
+    /**
+     *Variables booleanas
+     */
+
+    //Booleanas
+    val boolExample:Boolean = true;
+
+
+    //Operaciones
+    println("Sumar")
+    println(entero+decimal) //+,-,*,/,%
+
+    var exmapleSuma: Int = entero + decimal.toInt()
+
+
+
+
+}
