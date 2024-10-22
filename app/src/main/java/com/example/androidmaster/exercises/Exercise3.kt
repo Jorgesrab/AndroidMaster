cf283bdcb14c92f33078ef37e8f0a0b7fee33245
DiegoVillalba10 <diego.villalba2@educa.madrid.org>
2024-10-22 11:51:08 +0200

    Inclusion de ejemplos de Array en Kotlin.

--- app/src/main/java/com/example/androidmaster/exercises/Exercise3.kt
+++ app/src/main/java/com/example/androidmaster/exercises/Exercise3.kt
@@ -1,3 +1,28 @@
 package com.example.androidmaster.exercises
 
-fun
+fun main() {
+    var name: String = "Diego"
+    var name1: String = "Jorge"
+    var name2: String = "Pablo"
+    var name3: String = "Pedro"
+    var name4: String = "Lucas"
+    var name5: String = "Antonio"
+
+    val diasSemana = arrayOf("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo")
+
+    println(diasSemana.size)
+
+    if (diasSemana.size >= 8) {
+        println(diasSemana[7])
+    } else {
+        println("No hay más valores en el array.")
+    }
+
+    for (position in diasSemana.indices) {
+        println(diasSemana[position])
+    }
+
+    for ((position, value) in diasSemana.withIndex()) {
+        println("La posición $position tiene el valor $value.")
+    }
+}
