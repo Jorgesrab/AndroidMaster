6c1686a9580d19f2cd1bab282dba11fe9cfd4d2b
Jorgesrab <jorge.sanchez174@educa.madrid.org>
2024-10-28 13:26:05 +0100

    coomit

--- app/src/main/java/com/example/androidmaster/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/example/androidmaster/firstapp/FirstAppActivity.kt
@@ -1,10 +1,12 @@
 package com.example.androidmaster.firstapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+
 import com.example.androidmaster.R
 
 class FirstAppActivity : AppCompatActivity() {
@@ -12,10 +14,23 @@ class FirstAppActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_first_app)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnStart = findViewById<AppCompatButton>(R.id.btdStart)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+
+
+        btnStart.setOnClickListener {
+            val name = etName.text.toString()
+            if (name.isNotEmpty()) {
+                Log.i("Jorge", "Button Pulsado ${etName.text.toString()}")
+
+            }
+
+
         }
+
     }
+
+
 }
